b1e02a4966070cf6f317eb9aace2b76443c6f006
Sachin Hegde <SachinHegde328@gmail.com>
2025-07-03 03:04:11 +0000

    Paste a valid commit reference to create a new deployment in addition to

--- next.config.ts
+++ next.config.ts
@@ -26,11 +26,6 @@ const nextConfig: NextConfig = {
     '@octokit/rest',
     '@notionhq/client',
   ],
-  devIndicators: {
-    allowedDevOrigins: [
-        'https://6000-firebase-studio-1751210223860.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev'
-    ]
-  }
 };
 
 export default nextConfig;
