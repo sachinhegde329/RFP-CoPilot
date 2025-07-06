5c55fcd1b41c01323bce133f31c20aab0dedaa55
Sachin Hegde <SachinHegde328@gmail.com>
2025-07-06 07:16:04 +0000

    solve this issue

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,9 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
+    googleAI(),
   ],
   model: 'googleai/gemini-2.0-flash',
 });
