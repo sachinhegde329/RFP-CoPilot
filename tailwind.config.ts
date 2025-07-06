8312ef53559d52c9cb61917b44b46488835b967e
Sachin Hegde <SachinHegde328@gmail.com>
2025-07-06 14:40:53 +0000

    improve animations. the design should be like IOS app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,10 +87,25 @@ export default {
             height: '0',
           },
         },
+        'dialog-in': {
+          '0%': { opacity: '0', transform: 'translate(-50%, -48%) scale(0.96)' },
+          '100%': { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+        },
+        'dialog-out': {
+          '0%': { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+          '100%': { opacity: '0', transform: 'translate(-50%, -48%) scale(0.96)' },
+        },
+        'fade-in-scale-up': {
+            '0%': { opacity: '0', transform: 'scale(0.95)' },
+            '100%': { opacity: '1', transform: 'scale(1)' },
+        },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down 0.2s ease-in-out',
+        'accordion-up': 'accordion-up 0.2s ease-in-out',
+        'dialog-in': 'dialog-in 0.2s ease-out',
+        'dialog-out': 'dialog-out 0.2s ease-out',
+        'fade-in-scale-up': 'fade-in-scale-up 0.1s ease-out',
       },
       typography: (theme: (arg0: string) => any) => ({
         DEFAULT: {
