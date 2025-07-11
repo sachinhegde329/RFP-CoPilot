baed03895a3ea5ad6e6ef821c7ccbf9e7cfb2886
Sachinhegde329 <sachinhegde329@gmail.com>
2025-07-11 13:02:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   serverExternalPackages: [
